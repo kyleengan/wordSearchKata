8de3564748dc45d7cdca259ba60bc3f4b626b6fb
Kyle Engan <kyle.engan@gmail.com>
2017-07-16 12:39:15 -0400

    Added gitignore (oops), added tests and code for reading word list from puzzle file

--- src/main/java/WordSearch.java
+++ src/main/java/WordSearch.java
@@ -2,13 +2,25 @@ import java.io.*;
 
 public class WordSearch {
 
+    String[] wordList = null;
+
     public WordSearch() {
 
     }
 
-    public void readFile(String filepath) throws FileNotFoundException {
+    public void readFile(String filepath) throws FileNotFoundException, IOException {
         File puzzleFile = new File(filepath);
         BufferedReader bufferedReader = new BufferedReader(new FileReader(puzzleFile));
+
+        String wordLine = bufferedReader.readLine();
+
+        if (wordLine != null && !wordLine.isEmpty()) {
+            wordList = wordLine.split(",+");
+        }
+    }
+
+    public String[] getWordList() {
+        return wordList;
     }
 
     public static void main(String[] args) {
--- src/test/java/WordSearchTest.java
+++ src/test/java/WordSearchTest.java
@@ -1,6 +1,7 @@
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -17,19 +18,56 @@ public class WordSearchTest {
     public void setup() throws Exception {
         subject = new WordSearch();
 
-        URL resource = WordSearch.class.getResource("wordsearch.csv");
-        puzzleFilePath = Paths.get(resource.toURI()).toFile().getAbsolutePath();
     }
 
+    private void setPuzzleFilePath(String filename) {
+        URL resource = WordSearch.class.getResource(filename + ".csv");
+        try {
+            puzzleFilePath = Paths.get(resource.toURI()).toFile().getAbsolutePath();
+        } catch (Exception e) {
+            fail("Could not find file " + filename + ".csv");
+        }
+    }
 
 
     @Test
     public void canReadCSVFileFromProvidedFilePath() {
+        setPuzzleFilePath("wordsearch");
+
         try {
             subject.readFile(puzzleFilePath);
         } catch (Exception e) {
             fail("Application threw an exception attempting to read puzzle file.");
         }
+    }
+
+    @Test
+    public void canReadWordListFromCSVFile() throws Exception {
+        setPuzzleFilePath("wordsearch");
+        String[] wordList = {"BONES","KHAN","KIRK","SCOTTY","SPOCK","SULU","UHURA","CHEKHOV"};
+
+        subject.readFile(puzzleFilePath);
+
+        Assert.assertArrayEquals(wordList, subject.getWordList());
+    }
+
+    @Test
+    public void canReadWordListFromCSVFileWithExtraCommasBecauseReasons() throws Exception {
+        setPuzzleFilePath("wordsearchMalformedInput");
+        String[] wordList = {"BONES","KHAN","KIRK","SCOTTY","SPOCK","SULU","UHURA","CHEKHOV"};
+
+        subject.readFile(puzzleFilePath);
+
+        Assert.assertArrayEquals(wordList, subject.getWordList());
+    }
+
+    @Test
+    public void cannotReadWordListWithNoWords() throws Exception {
+        setPuzzleFilePath("wordsearchNoWords");
+
+        subject.readFile(puzzleFilePath);
+
+        Assert.assertNull(subject.getWordList());
     }
 
 
